9e8fbaf6dc378933d6e36036a34564bce1754c8e
zzq <2044579211@qq.com>
2018-05-23 18:10:51 +0800

    eureka集群优化

--- eureka-server/src/main/java/zzq/eureka/EurekaApplication.java
+++ eureka-server/src/main/java/zzq/eureka/EurekaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaClient
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication{
