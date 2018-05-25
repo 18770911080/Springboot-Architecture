835a310e708e9d750374adf33ae9556eb2cfd9a2
zzq <2044579211@qq.com>
2018-05-25 18:03:20 +0800

    ribbon配置hystrix断路器回调

--- ribbon-server/src/main/java/zzq/ribbon/RibbonApplication.java
+++ ribbon-server/src/main/java/zzq/ribbon/RibbonApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import zzq.ribbon.service.HelloService;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 @RestController
 public class RibbonApplication {
 
