a711c1212208fd7626d3ea72e7b70856fb4b9bbb
zzq <2044579211@qq.com>
2018-05-28 14:23:17 +0800

    feign断路器配置

--- feign-server/src/main/java/zzq/feign/service/HelloService.java
+++ feign-server/src/main/java/zzq/feign/service/HelloService.java
@@ -3,7 +3,7 @@ package zzq.feign.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("eureka-server")
+@FeignClient(name="ribbon",fallback = HelloServiceHystrix.class)
 public interface HelloService {
 
     @RequestMapping("hello")
