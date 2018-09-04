801fc94546d945c61a1e1baa704c287d00c2c07a
zzq <2044579211@qq.com>
2018-09-04 14:10:57 +0800

    工具

---
+++ zqtool/src/test/java/zzq/zqtool/ZqtoolApplicationTests.java
@@ -0,0 +1,16 @@
+package zzq.zqtool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZqtoolApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
